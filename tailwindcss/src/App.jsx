a0e8272c5eecf67af44d6a260f4e465ad711e327
Alok Yadav <alokyadav4021@gmail.com>
2026-06-01 15:43:54 +0530

    this is the first small tailwind css code that i have applied on my page im still learning it

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <div className="bg-amber-300 m-5">
+        <p className="text-xl font-medium text-center text-rose-600">
+          Hello World
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
